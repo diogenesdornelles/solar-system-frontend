7d377e2f296d73159a6982c73422624914a7c03d
diogenesdornelles <diogenes.dornelles@gmail.com>
2023-08-02 13:26:06 -0300

    fix img-bg path

--- src/components/Home/background/Background.jsx
+++ src/components/Home/background/Background.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { AppContext } from '../../../context/AppContext';
-import svg from '../../../assets/images/Home/frame4k.svg'
+// import svg from '../../../assets/images/Home/frame4k.svg'
 
 function Background() {
   const appContext = useContext(AppContext);
@@ -10,7 +10,7 @@ function Background() {
   return (
     <>
       <img
-        className={`w-screen h-screen absolute top-0 left-0 bg-[url(${svg})] bg-no-repeat ${
+        className={`w-screen h-screen absolute top-0 left-0 bg-[url(../../../assets/images/Home/frame4k.svg)] bg-no-repeat ${
           gameStarted ? 'bg-left' : ''
         } animated-bg transition-all duration-1000 -z-[9999]`}
       />
