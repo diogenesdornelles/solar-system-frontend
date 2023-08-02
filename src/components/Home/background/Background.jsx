706c534c369a4a1fca97875cd324691a0a6283ca
diogenesdornelles <diogenes.dornelles@gmail.com>
2023-08-02 13:59:33 -0300

    fix-bg-2

--- src/components/Home/background/Background.jsx
+++ src/components/Home/background/Background.jsx
@@ -10,10 +10,12 @@ function Background() {
   return (
     <>
       <img
-        className={`w-screen h-screen absolute top-0 left-0 bg-[url(../../../assets/images/Home/frame4k.svg)] bg-no-repeat ${
+        className={`w-screen h-screen absolute top-0 left-0 bg-no-repeat ${
           gameStarted ? 'bg-left' : ''
         } animated-bg transition-all duration-1000 -z-[9999]`}
-      />
+        style={{
+          backgroundImage: "url('../../../assets/images/home/frame4k.svg')"
+        }}/>
     </>
   );
 }
